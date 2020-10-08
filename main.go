c411b426105f59a3fff43f6cbcc65e823b71c5bc
Know1 <github@lukebutcher.com>
2020-10-08 20:29:00 +1100

    Update main.go

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 }
 
 func HelloServer(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello World. Path: %s", r.URL.Path[1:])
+  fmt.Fprintf(w, "Hello World\nPath: %s", r.URL.Path[1:])
 }
